2e44dffe2d454c39d582c5d31e129171225ea6fc
simperiale <ssimperiale@gmail.com>
2024-12-20 13:02:26 -0300

    key-pair

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 ##Create and bootstrap webserver #lanzar
+##key_name                    = "temp"
 resource "aws_instance" "webserver" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
-  key_name                    = "temp"
+  #key_name                    = "temp"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
